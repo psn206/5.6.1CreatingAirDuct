dadbb7abab04b889f388a009b233b4f8a921f59f
Sergei Prutsakov <psn206@mail.ru>
2024-08-27 12:54:50 +0300

    Добавьте файлы проекта.

---
+++ CreatingAirDuct/MainViewViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingAirDuct
+{
+    internal class MainViewViewModel
+    {
+        MethodRevitApp methodRevitApp;
+        public DelegateCommand SaveCommand { get; }
+        public List<DuctType> TypeDuct { get; set; } = new List<DuctType>();
+        public DuctType SelectedTypeDuct { get; set; }
+        public List<MEPSystemType> TypeSystem { get; set; } = new List<MEPSystemType>();
+        public MEPSystemType SelectedTypeSystem { get; set; }
+        List<XYZ> Points { get; } = new List<XYZ>();
+        public List<Level> Levels { get; set; } = new List<Level>();
+        public Level SelectedLevel { get; set; }
+        public string Offset { get; set; }
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            SaveCommand = new DelegateCommand(OnSaveCommand);
+            methodRevitApp = new MethodRevitApp(commandData);
+            TypeSystem = methodRevitApp.GetSystemTypes();
+            TypeDuct = methodRevitApp.GetDuctTypes();
+            Levels = methodRevitApp.GetListLevel();
+            Points = methodRevitApp.GetPoints();
+        }
+
+        private void OnSaveCommand()
+        {
+            methodRevitApp.CreateDuct(Points, SelectedLevel, SelectedTypeSystem, SelectedTypeDuct, Offset);
+            RaiseCloseRecuest();
+        }
+
+        public event EventHandler CloseRecuest;
+
+        public void RaiseCloseRecuest()
+        {
+            CloseRecuest?.Invoke(this, EventArgs.Empty);
+        }
+
+    }
+}
---
+++ CreatingAirDuct/MethodRevitApp.cs
@@ -0,0 +1,105 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingAirDuct
+{
+    internal class MethodRevitApp
+    {
+
+        UIApplication uiApp;
+        UIDocument uiDoc;
+        Document doc;
+
+        public UIApplication UiApp { get => uiApp; }
+        public UIDocument UiDoc { get => uiDoc; }
+        public Document Doc { get => doc; }
+
+        public MethodRevitApp(ExternalCommandData commandData)
+        {
+            uiApp = commandData.Application;
+            uiDoc = UiApp.ActiveUIDocument;
+            doc = UiDoc.Document;
+        }
+
+        public List<MEPSystemType> GetSystemTypes()
+        {
+            List<MEPSystemType> systemTypes = new FilteredElementCollector(Doc)
+                 .OfClass(typeof(MEPSystemType))
+                 .Cast<MEPSystemType>()
+                 .ToList();
+            return systemTypes;
+        }
+
+
+        public List<DuctType> GetDuctTypes()
+        {
+            var ductTypes =
+                new FilteredElementCollector(doc)
+                    .OfClass(typeof(DuctType))
+                    .Cast<DuctType>()
+                    .ToList();
+            return ductTypes;
+        }
+
+        public List<Level> GetListLevel()
+        {
+            List<Level> levels = new FilteredElementCollector(doc)
+                                                .OfClass(typeof(Level))
+                                                .Cast<Level>()
+                                                .ToList();
+            return levels;
+        }
+
+        public List<XYZ> GetPoints()
+        {
+            List<XYZ> points = new List<XYZ>();
+            while (true)
+            {
+                XYZ pickedPoint = null;
+                try
+                {
+                    pickedPoint = UiDoc.Selection.PickPoint(ObjectSnapTypes.Endpoints, "Выберете точку");
+                }
+                catch (Autodesk.Revit.Exceptions.OperationCanceledException ex)
+                {
+                    break;
+                }
+                points.Add(pickedPoint);
+            }
+            return points;
+        }
+
+        public void CreateDuct(List<XYZ> points, Level selectedLevel, MEPSystemType selectedMEPSystemType, DuctType selectedTypeDuct, string offset)
+        {
+            if (points.Count == 0 || selectedLevel == null || selectedTypeDuct == null || selectedMEPSystemType == null) return;
+            double setCentrLine = Convert.ToDouble(offset);
+
+            using (var ts = new Transaction(Doc, "Create Duct"))
+            {
+                ts.Start();
+                for (int i = 0; i < points.Count; i++)
+                {
+                    if (i == 0) continue;
+                    XYZ startPoint = points[i - 1];
+                    XYZ endPoint = points[i];
+                    Duct duct = Duct.Create(Doc, selectedMEPSystemType.Id, selectedTypeDuct.Id, selectedLevel.Id, startPoint, endPoint);
+                    Parameter parameter = duct.get_Parameter(BuiltInParameter.MEP_UPPER_CENTERLINE_ELEVATION);
+                    parameter.Set(UnitUtils.ConvertToInternalUnits(setCentrLine, UnitTypeId.Millimeters));
+                }
+
+                ts.Commit();
+            }
+
+        }
+
+
+    }
+}
